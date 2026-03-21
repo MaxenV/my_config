8a828317009951f8f18ae15e94178ba2b0d8d176
MaxenV <maxenv@MaxenWin.localdomain>
2026-03-21 13:06:49 +0100

    Base kitty insall

---
+++ programs/kitty/main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# sudo apt install kitty
+
+mkdir -p ~/.config/kitty
+cp config/kitty.conf ~/.config/kitty/kitty.conf 
+cp config/GruvBox_DarkHard.conf ~/.config/kitty/GruvBox_DarkHard.conf 
