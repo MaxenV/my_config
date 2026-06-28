35987a9d0a191deb86ccd10d63625f68113a2a70
maxenv <maxen2001@gmail.com>
2026-06-28 16:46:31 +0200

    Fix fish issues

--- programs/fish/config/config.fish
+++ programs/fish/config/config.fish
@@ -1,6 +1,8 @@
-set -gx PATH /home/maxen/.local/bin $PATH
+set -gx PATH "$HOME/.local/bin" $PATH
 
-oh-my-posh init fish --config ~/.cache/oh-my-posh/themes/custom_quick-term.omp.json | source
+if type -q oh-my-posh
+    oh-my-posh init fish --config "$HOME/.cache/oh-my-posh/themes/custom_quick-term.omp.json" | source
+end
 
 
 if status is-interactive
@@ -9,13 +11,13 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/maxen/miniforge3/bin/conda
-    eval /home/maxen/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+if test -f "$HOME/miniforge3/bin/conda"
+    eval "$HOME/miniforge3/bin/conda" "shell.fish" "hook" $argv | source
 else
-    if test -f "/home/maxen/miniforge3/etc/fish/conf.d/conda.fish"
-        . "/home/maxen/miniforge3/etc/fish/conf.d/conda.fish"
+    if test -f "$HOME/miniforge3/etc/fish/conf.d/conda.fish"
+        . "$HOME/miniforge3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/home/maxen/miniforge3/bin" $PATH
+        set -x PATH "$HOME/miniforge3/bin" $PATH
     end
 end
 # <<< conda initialize <<<
--- programs/fish/main.sh
+++ programs/fish/main.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-sudo apt install -y fish
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+FISH_BIN="$(command -v fish)"
+
+sudo apt update
+sudo apt install -y curl fish
 curl -s https://ohmyposh.dev/install.sh | bash -s
-export PATH=$PATH:~/.local/bin
+export PATH="$PATH:$HOME/.local/bin"
 
 mkdir -p ~/.config/fish
-ln -f config/config.fish ~/.config/fish/config.fish
-ln -f config/custom_quick-term.omp.json ~/.cache/oh-my-posh/themes/custom_quick-term.omp.json
+mkdir -p "$HOME/.cache/oh-my-posh/themes"
+ln -sf "$SCRIPT_DIR/config/config.fish" ~/.config/fish/config.fish
+ln -sf "$SCRIPT_DIR/config/custom_quick-term.omp.json" "$HOME/.cache/oh-my-posh/themes/custom_quick-term.omp.json"
 
-oh-my-posh init fish --config /root/.cache/oh-my-posh/themes/custom_quick-term.omp.json
+oh-my-posh init fish --config "$HOME/.cache/oh-my-posh/themes/custom_quick-term.omp.json" | source
 
-oh-my-posh font install Hack
+# oh-my-posh font install Hack
 
-# oh-my-posh init fish --config ~/.cache/oh-my-posh/themes/custom_quick-term.omp.json | source
-eval "$(oh-my-posh init bash --config ~/.cache/oh-my-posh/themes/custom_quick-term.omp.json)"
-chsh -s "$(which fish)"
+echo "Fish is installed at: $FISH_BIN"
+echo "Set your login shell manually if you want fish to start on login: chsh -s \"$FISH_BIN\""
 
